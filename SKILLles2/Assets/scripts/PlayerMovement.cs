bbe9c1a76a05f772f8117b4720580d8db1ecf8ec
Timothy Kuijper <32886@ma-web.nl>
2023-02-23 12:26:25 +0100

    assignments added

---
+++ SKILLles2/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = transform.position + new Vector3(0.5f, 0, 0);
+        }                                                                 
+        if (Input.GetKey(KeyCode.A))                                      
+        {                                                                 
+            transform.position = transform.position + new Vector3(-0.5f, 0, 0);
+        }                                                                 
+        if (Input.GetKey(KeyCode.Space))                                  
+        {                                                                 
+            transform.position = transform.position + new Vector3(0, 0.5f, 0);
+        }
+    }
+}
